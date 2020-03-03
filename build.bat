642675fffadb081f53095b42ece66c13d92b1004
inponomarev <iponomarev@mail.ru>
2020-03-03 18:42:17 +0300

    update

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-docker run --rm -v %cd%:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor-revealjs -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.8.0 -r asciidoctor-diagram -o index.html -D /documents/output lecture.adoc
+docker run --rm -v %cd%:/documents/ -w /documents --name asciidoc-to-html asciidoctor/docker-asciidoctor  asciidoctor-revealjs -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.8.0 -r asciidoctor-diagram -o index.html -D /documents/output lecture.adoc
 copy /Y white_course.css output\white_course.css
 xcopy /I /Y images output\images
 xcopy /I /Y font-awesome-4.7.0 output\font-awesome-4.7.0
